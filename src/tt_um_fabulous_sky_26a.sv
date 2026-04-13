1002e58eba84343bf0e0e7142b668ad44445ecba
Leo Moser <leomoser99@gmail.com>
2026-04-13 09:23:26 +0200

    fix: add power pins to verilog
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- src/tt_um_fabulous_sky_26a.sv
+++ src/tt_um_fabulous_sky_26a.sv
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module tt_um_fabulous_sky_26a (
+    `ifdef USE_POWER_PINS
+    inout VPWR,
+    inout VGND,
+    `endif
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
